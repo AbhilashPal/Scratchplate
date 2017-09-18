208fe6c9e38d89a85cf694f013824e9f0f46fbea
Abhilash Pal <abhilashpal8@gmail.com>
2017-09-18 21:04:08 +0530

    Update Scratch.cpp

--- Scratch.cpp
+++ Scratch.cpp
@@ -1,20 +1,58 @@
 /*
-  Print elements of a linked list on console 
-  head pointer input could be NULL as well for empty list
-  Node is defined as 
-  struct Node
-  {
+    Insert Node in a doubly sorted linked list 
+    After each insertion, the list should be sorted
+   Node is defined as
+   struct Node
+   {
      int data;
-     struct Node *next;
-  }
+     Node *next;
+     Node *prev;
+   }
 */
-void Print(Node *head)
+Node* SortedInsert(Node *head,int data)
 {
-  Node* temp=new Node;
-  temp=head;
-  while(temp->next!=NULL)
-  {
-      cout<<temp->data<<endl;
-      temp=temp->next;
-  }
+    Node* temp=new Node;
+    temp->next=NULL;
+    temp->prev=NULL;
+    Node* newnode=new Node;
+    newnode->data=data;
+    newnode->next=NULL;
+    newnode->prev=NULL;
+    temp=head;
+    if(temp==NULL)
+    {
+       temp=newnode;   
+       return head; 
+    }
+    else if(temp->next=NULL)
+    {
+      if(temp->data<data)
+      {
+        temp->next=newnode;
+        newnode->prev=temp;
+        return head;  
+      }
+      else
+      {
+        temp->prev=newnode;
+        newnode->next=temp;
+        head=newnode;
+        return head;
+      }
+    }
+    else
+    {
+      while(temp->next!=NULL&&temp->data<data)
+      {
+        temp=temp->next;
+      }
+      newnode->prev=temp;
+      if(temp->next!=NULL)
+      {
+        newnode->next=temp->next;
+        temp->next->prev=newnode;
+      }
+      temp->next=newnode;
+      return head;
+    }
 }

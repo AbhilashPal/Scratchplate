9161e0cf53110faefd4e5f7921f1c6912997647b
Abhilash Pal <abhilashpal8@gmail.com>
2017-10-16 20:07:55 +0530

    Update Verilog.v

--- Verilog.v
+++ Verilog.v
@@ -1,21 +1,27 @@
-module Railgate(a,b,c,d,x,y);
-	input a,b,c,d;
-	output x,y;
-	x=or(a,b);
-	y=or(c,d);
+module TrainGate(A,B,C,D,Gx,Gy);
+	input A,B,C,D;
+	output Gx,Gy;
+	or  G1(Gx,A,B);
+	or  G2(Gy,C,D);
 endmodule
-module testbench;
-	reg a,b,c,d;
-	Railgate DUT(a,b,c,d,x,y);
 
-	initial 
+module testbench;
+	reg A,B,C,D;
+	wire Gx,Gy;
+	TrainGate DUT(A,B,C,D,Gx,Gy);
+	initial
 		begin
-			$monitor($time,"A=%b,B=%b,C=%b,D=%b,X=%b,Y=%b");
-			#5 a=1;b=0;c=0;d=1;
-			#5 a=1;b=1;c=0;d=1;
-			#5 a=0;b=0;c=1;d=0;
-			#5 a=1;b=0;c=0;d=0;
-			#5 a=0;
+			$monitor($time,"   A=%b,B=%b,C=%b,D=%b    Gx=%b,Gy=%b",A,B,C,D,Gx,Gy);
+			$dumpfile("traingate.vcd");
+			$dumpvars(0,testbench);
+			#5 A=1;B=0;C=0;D=1;
+			#5 A=0;B=0;C=1;D=1;
+			#5 A=1;C=0;
+			#5 A=1;D=0;
+			#5 B=0;C=1;
+			#5 A=0;B=0;C=0;D=1;
+			#5 A=0;B=1;C=1;D=0;
+			#5 B=0;C=0;
 			#5 $finish;
 		end
 endmodule
